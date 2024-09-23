f3698871d3615a15535ada3233903b452097220c
micah <wariomastergg@gmail.com>
2024-09-23 14:12:56 -0600

    edited assignment1

--- sandbox/assignment1/Program.cs
+++ sandbox/assignment1/Program.cs
@@ -8,7 +8,7 @@ class Program
         String first = Console.ReadLine();
         Console.Write("What is your last name? ");
         String last = Console.ReadLine();
-        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
         
         
     }
