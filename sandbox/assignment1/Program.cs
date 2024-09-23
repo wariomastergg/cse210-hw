82dc4636f053ceac804fb95af08ca9ba6e5ff228
micah <wariomastergg@gmail.com>
2024-09-23 14:09:37 -0600

    added assignment1 in sandbox folder

---
+++ sandbox/assignment1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        String first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+        
+        
+    }
+}
