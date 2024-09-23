e53e676e1024ed7e0acdc563fa2692a6363c7183
micah <wariomastergg@gmail.com>
2024-09-23 14:16:44 -0600

    edited prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        
+        
     }
 }
