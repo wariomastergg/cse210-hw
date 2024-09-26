8c418c7594b2d31e62fd53cba607dc0e520c0d8a
micah <wariomastergg@gmail.com>
2024-09-26 11:34:28 -0600

    finished project 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //make list for numbers
+        List<int> nums = new();
+        int number = -1;
+        while (number != 0){
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0){ break; }
+            nums.Add(number);
+        }
+
+        int sum = 0;
+        int most = nums[0];
+        foreach ( int i in nums ){
+            sum += i;
+            //Console.WriteLine(i);
+            if ( i > most ){
+                most = i;
+            }
+
+        }
+     
+
+        
+
+
+        Console.WriteLine($"the sum is: {sum}");
+        Console.WriteLine($"the average is: {(double)(sum/nums.Count)}");
+        Console.WriteLine($"the largest number is: {most}");
+
+
+        //get length of list
+        int length = nums.Count;
+
+
+        
+        
     }
 }
