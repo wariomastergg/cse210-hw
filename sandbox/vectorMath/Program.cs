0bc90e48073f4b85ca5399af555f61dd48cebb61
micah <wariomastergg@gmail.com>
2024-12-07 16:56:45 -0700

    worked on vector classes and added visual library. my progress is currently in thye sandbox folder under vector math. it looks like i havent done much but the bulk of ity is done and it should be sinch to get the rest done

--- sandbox/vectorMath/Program.cs
+++ sandbox/vectorMath/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Raylib_cs;
 
 using static Vec2;
 
@@ -6,12 +7,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Vec2 a = vec2( 2.0, 6.0 );
-        Vec2 b = vec2( 3.0, 5.0 );
 
-        Console.WriteLine( a._r );
-        Console.WriteLine( (2.0 * a)._r );
-        //Console.WriteLine( (a + b)._g );
-        Console.WriteLine( a._r );
+        Raylib.InitWindow(800, 480, "Hello World");
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            Raylib.DrawRectangle(100, 100, 10, 10, Color.Black);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+
+        //Vec2 b = vec2( 3.0, 5.0 );
+        //Vec2 a = vec2( 2.0, 6.0 );
+        //Console.WriteLine( a._r );
+        //Console.WriteLine( (2.0 * a)._r );
+        ////Console.WriteLine( (a + b)._g );
+        //Console.WriteLine( a._r );
     }
 }
